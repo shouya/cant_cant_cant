660c6c6372dc7fd186feabcf3de4fe915e18e6e2
Shou Ya <github@lain.li>
2016-09-27 14:27:04 +0800

    make it working!

--- lib/cant_cant_cant.rb
+++ lib/cant_cant_cant.rb
@@ -5,69 +5,71 @@ module CantCantCant
   InvalidControllerOrAction = Class.new(RuntimeError)
   InvalidConfiguration = Class.new(RuntimeError)
 
-  CONFIG_FILE = File.join('x', 'config/cant_cant_cant.yml').freeze
+  class << self
+    def initialize(config)
+      @config_file = config
+      @cache = {}
 
-  def inject_actions
-    validate_config
-
-    permission_table.values.map(&:keys).flatten.uniq.each do |param|
-      inject_action(param)
+      inject_actions
     end
-  end
 
-  def allow?(param, roles)
-    return false if roles.empty?
-    roles.each do |role|
-      role_spec = permission_table[role]
-      next if role_spec[param].empty?
-      next if role_spec[param] == 'deny'
-      return true if role_spec[param] == 'allow'
-    end
-    false
-  end
+    def inject_actions
+      validate_config
 
-  def permissoins_for(roles)
-    permission_table
-      .values_at(*roles)
-      .map { |x| x.keep_if { |_, v| v == 'allow' }.keys }
-      .flatten
-      .uniq
-  end
-
-  private
-
-  def permission_table
-    @permission_table ||= YAML.load_file(CONFIG_FILE).freeze
-  end
-
-  def extract_controller(param)
-    controller, action = param.split('#')
-    raise if controller.empty? || action.empty?
-
-    controller = (controller.classify + 'Controller').constantize
-    raise unless controller.instance_methods(false).map(&:to_s).include? action
-
-    [controller, action]
-  rescue RuntimeError, NameError
-    raise InvalidControllerOrAction, param
-  end
-
-  def inject_action(param)
-    controller, action = extract_controller(param)
-    controller.class_eval do
-      set_callback(action, :before) do
-        next true if allowed?(param, current_roles)
-        raise PermissionDenied, param
+      permission_table.values.map(&:keys).flatten.uniq.each do |param|
+        inject_action(param)
       end
     end
-  end
 
-  def validate_config
-    permission_table.each do |_, perms|
-      perms.each do |param, access|
-        next unless access.empty?
-        next unless access.in? %w(allow deny)
-        raise InvalidConfiguration, param
+    def allow?(param, roles)
+      permissions_for(roles).include? param
+    end
+
+    def permissions_for(roles)
+      @cache[roles.sort.join(',')] ||=
+        permission_table
+        .values_at(*roles)
+        .map(&:present?)
+        .map { |x| x.keep_if { |_, v| v == 'allow' }.keys }
+        .flatten
+        .uniq
+    end
+
+    private
+
+    def permission_table
+      @permission_table ||= YAML.load_file(@config_file).freeze
+    end
+
+    def extract_controller(param)
+      controller, action = param.split('#')
+      raise if controller.blank? || action.blank?
+
+      controller = (controller.classify + 'Controller').constantize
+      raise unless action.in? controller.instance_methods(false).map(&:to_s)
+
+      [controller, action]
+    rescue RuntimeError, NameError
+      raise InvalidControllerOrAction, param
+    end
+
+    def inject_action(param)
+      controller, action = extract_controller(param)
+      controller.class_eval do
+        before_action(only: [action]) do
+          next true if CantCantCant.allow?(param, current_roles)
+          raise PermissionDenied, param
+        end
+      end
+    end
+
+    def validate_config
+      permission_table.each do |_, perms|
+        perms.each do |param, access|
+          next unless access.blank?
+          next unless access.in? %w(allow deny)
+          raise InvalidConfiguration, param
+        end
       end
     end
   end
--- lib/generators/cant_cant_cant/install/templates/initializer.rb
+++ lib/generators/cant_cant_cant/install/templates/initializer.rb
@@ -1,4 +1,12 @@
+config = File.join(Rails.root, 'config/cant_cant_cant.yml')
+CantCantCant.initialize(config)
+
 class ActionController::Base
+  # Write your own handler
+  rescue_from CantCantCant::PermissionDenied do
+    render plain: 'permission denied', status: 403
+  end
+
   # Write your own method to return the roles for current user
   def current_roles
     return [] unless defined? current_user
